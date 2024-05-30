adf3a1497a048d51d80fba250e43fecc40be42dc
Srujan-rai <raisrujan@gmail.com>
2024-05-30 22:29:04 +0530

    cors added

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 model = load_model("best_model.h5")
-cors(app)
+CORS(app)
 
 data_tuple = (
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
